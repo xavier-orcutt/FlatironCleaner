cb13ac829fe3e27dbb23075f5530d6311e87704a
Xavier Orcutt <84699274+xavier-orcutt@users.noreply.github.com>
2025-02-28 16:39:53 -0700

    Create __init__.py

---
+++ flatiron_cleaner/__init__.py
@@ -0,0 +1,26 @@
+"""
+FlatironCleaner
+
+A Python package for cleaning and harmonizing Flatiron Health cancer data
+structured around specified index dates of interest.
+"""
+
+__version__ = '0.1.0'
+
+# Make key classes available at package level
+from .urothelial import DataProcessorUrothelial
+from .nsclc import DataProcessorNSCLC
+from .colorectal import DataProcessorColorectal
+from .breast import DataProcessorBreast
+from .prostate import DataProcessorProstate
+from .merge_utils import merge_dataframes
+
+# Define what gets imported with `from flatiron_cleaner import *`
+__all__ = [
+    'DataProcessorUrothelial',
+    'DataProcessorNSCLC',
+    'DataProcessorColorectal',
+    'DataProcessorBreast',
+    'DataProcessorProstate',
+    'merge_dataframes',
+]
